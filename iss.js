eb41577145db8a1cef868039561309300975749d
Atkinss1 <jordanatkins@live.com>
2023-10-20 22:12:19 -0600

    add: fetchCoordsByIP function, takes in IP to find longitude/latitude

--- iss.js
+++ iss.js
@@ -1,5 +1,5 @@
 const request = require('request');
-/**
+/*
  * Makes a single API request to retrieve the user's IP address.
  * Input:
  *   - A callback (to pass back an error or the IP string)
@@ -10,8 +10,10 @@ const request = require('request');
 
 
 const fetchMyIP = function(callback) {
+  // IP fetching API url
   const myIP = 'https://api.ipify.org?format=json';
   request(myIP, (error, response, body) => {
+    // if statusCode is not 200, deliver a message
     if (response.statusCode !== 200) {
       callback(Error(`Status code ${response.statusCode} when fetching IP. Response: ${body}`), null);
       return;
@@ -20,9 +22,37 @@ const fetchMyIP = function(callback) {
       callback(error, 'That didn\'t work!');
       return;
     }
+    // filter JSON for IP variable
     const data = JSON.parse(body).ip;
     callback(null, data);
   });
 };
 
-module.exports = fetchMyIP;
+
+/*
+ * fetchCoordsByIP
+ * takes in an IP address and returns the latitude and longitude for it.
+ */
+
+const fetchCoordsByIP = function(ip, callback) {
+  // test url not implementing Ip parameter yet
+  const url = 'http://ipwho.is/';
+  request(url, (error, response, body) => {
+    // if statusCode throws an error, deliver a message
+    if (response.statusCode !== 200) {
+      callback(Error(`Status code ${response.statusCode} when fetching coords. Response: ${body}`));
+      return;
+    }
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    callback(null, body);
+  });
+};
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP
+};
+  
