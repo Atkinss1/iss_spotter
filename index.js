e23a819a4258e8843577173a18ae125f0658d3bf
Atkinss1 <jordanatkins@live.com>
2023-10-20 22:13:11 -0600

    add: fetchCoordsByIP call, filters longitude/latitude and console.logs

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const fetchMyIP = require('./iss');
-
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+ // fetchMyIP takes in the local IP address of user from an API request
 fetchMyIP((error, ip) => {
   if (error) {
     console.log('That didn\'t work!', error);
     return;
-  }
+  } // returns IP in message
   console.log('It worked! Returned IP: ', ip);
 });
+
+// test IP for functionality testing
+fetchCoordsByIP('96.52.110.15'
+  , (error, data) => {
+    if (error) {
+      console.log(error);
+      return;
+    } // Parse through JSON file that was requested by IP API
+    data = JSON.parse(data);
+    // filtering JSON for longitude and latitude
+    const latitude = data.latitude;
+    const longitude = data.longitude;
+    console.log({
+      latitude: latitude,
+      longitude: longitude
+    });
+  });
 
