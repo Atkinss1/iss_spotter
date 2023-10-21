4874946a1b3beec82055d298a3f404e86c499df1
Atkinss1 <jordanatkins@live.com>
2023-10-21 15:34:18 -0600

    refactor: time conversion function

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const { nextISSTimesForMyLocation, unixConversionPacific} = require('./iss');
+const { nextISSTimesForMyLocation, printPassTimes} = require('./iss');
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log('It didn\'t work!', error);
   }
   // looping through each item in the object and converting unix time to a date
-  for (const rotation of passTimes) {
-    let myDate = unixConversionPacific(rotation);
-    console.log(`Next pass at ${myDate} for ${rotation.duration} seconds!`);
-  }
+  printPassTimes(passTimes);
 });
