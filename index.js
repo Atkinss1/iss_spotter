d633e938c283ff2b323d4c5b4dceca5bf1d7a107
Atkinss1 <jordanatkins@live.com>
2023-10-20 22:46:19 -0600

    remove: JSON parsing, long/lat variables and console.log

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { fetchMyIP, fetchCoordsByIP } = require('./iss');
- // fetchMyIP takes in the local IP address of user from an API request
+// fetchMyIP takes in the local IP address of user from an API request
 fetchMyIP((error, ip) => {
   if (error) {
     console.log('That didn\'t work!', error);
@@ -9,19 +9,10 @@ fetchMyIP((error, ip) => {
 });
 
 // test IP for functionality testing
-fetchCoordsByIP('96.52.110.15'
-  , (error, data) => {
-    if (error) {
-      console.log(error);
-      return;
-    } // Parse through JSON file that was requested by IP API
-    data = JSON.parse(data);
-    // filtering JSON for longitude and latitude
-    const latitude = data.latitude;
-    const longitude = data.longitude;
-    console.log({
-      latitude: latitude,
-      longitude: longitude
-    });
-  });
-
+fetchCoordsByIP('96.52.110.15', (error, coordinates) => {
+  if (error) {
+    console.log(error, null);
+    return;
+  }
+  console.log('It worked! Return coordinates: ', coordinates);
+});
